d6be40c2df4a225fa5d536303472b519f07b06b4
Нисик <@DESKTOP-ALORV4C>
2023-04-17 13:38:13 +0300

    Добавьте файлы проекта.

---
+++ day 1(1)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double L, R, S;
+        const double PI = Math.PI;
+
+        Console.Write("Введите длину окружности L: ");
+        L = double.Parse(Console.ReadLine());
+
+
+        R = L / (2 * PI);
+
+
+        S = PI * R * R;
+
+        Console.WriteLine("Радиус окружности R = " + R);
+        Console.WriteLine("Площадь круга S = " + S);
+
+        Console.ReadKey();
+    }
+}
